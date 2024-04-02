d8a6d35cce1d0bdefab8d2ca681fe9ea0b4d5c0a
MaxUhl98 <frozen2ice@web.de>
2024-04-02 18:33:35 +0200

    feat: Added logger to file

--- FC-Res/fc_res_cnn.py
+++ FC-Res/fc_res_cnn.py
@@ -5,14 +5,37 @@ import torch
 from torch import nn
 from typing import Callable, Dict, Any, Union
 import numpy as np
-import pandas as pd
-from utils.helpers import get_logger
-from utils.engine import k_fold_train
-from data_generator import prepare_train, GenericData
 from torchsummary import summary
-from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
-from pprint import pformat
+import logging
 
+def get_logger(name: str, base_filepath: str = 'logs/model_experiments') -> logging.Logger:
+    """Creates a logging.Logger object that writes a logfile named name.log into the folder at base_filepath
+    (throws an error if the folder does not exist)
+
+    :param name: Name of the logger and the logging file
+    :param base_filepath: Path to the folder in which the logging file will get saved
+    :return: Logger object
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(f'{base_filepath}/{name}.log', encoding='utf-8')
+    fh.setLevel(logging.DEBUG)
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+
+    # add the handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    return logger
 
 class ResidualConvolution(nn.Module):
     def __init__(self, input_channels: int, kernel_size: int, inner_dim: int, pooler: Callable, pooler_kernel_size: int,
